eec68b0e48632a80124e379c2c1d8730f2f60793
Andrey Morozov <andre15@inbox.ru>
2018-07-08 20:08:08 +0400

    Определение продукции нулевой цены для соперника + плавное уменьшение моего предложения (с прибавлением)

--- src/myStrategy.js
+++ src/myStrategy.js
@@ -11,12 +11,28 @@ module.exports = class Agent {
             this.total += counts[i]*values[i];
 		
 		this.isFirstPlayer = false;
+		this.enemyZeroIndexes = []; 
     }
     offer(o){
         this.log(`${this.rounds} rounds left`);
-        this.rounds--;
+        
         if (o)
         {
+			var suggestedSumValue = 0;
+			for (var i = 0; i < o.length; ++i)
+				suggestedSumValue += o[i] * this.values[i];			
+			this.log(`suggested sum value: ${suggestedSumValue}`);
+
+			if (this.rounds == this.max_rounds)
+			{
+				for (let i = 0; i<o.length; i++){
+					if (o[i] == this.counts[i]){
+						//enemy don't need this item. Probably is has zero value for him
+						this.enemyZeroIndexes.push(i);
+					}
+				}
+			}
+
 			if (!this.isFirstPlayer && this.rounds == 1) 
 			{
 				let sum = 0;
@@ -47,18 +63,32 @@ module.exports = class Agent {
 			var index = -1;
 			var minValue = Number.MAX_VALUE;
 			
+			//find item with minimum value and decrement it in my offer
 			for (var i = 0; i < this.values.length; ++i)
 			{
 				if (o[i] == 0) continue;
+
+				//this item probably has zero value for my enemy
+				if (this.enemyZeroIndexes.includes(i)){
+					o[i] = this.counts[i];
+					continue;		
+				}
+
 				if (this.values[i] < minValue)
 				{
 					minValue = this.values[i];
 					index = i;
 				}
 			}
+
+			if (this.myLastOffer && this.values[this.myLastOfferIndex] < this.values[index]){
+				o[this.myLastOfferIndex]++;
+			}
 			
+			this.myLastOfferIndex = index;
 			o[index]--;	
 			
+			//if current offer if zero value for me, suggest preveous one
 			var isZeroOffer = true;
 			for (var i = 0; i < o.length; ++i)
 			{
@@ -70,11 +100,19 @@ module.exports = class Agent {
 			}
 			if (isZeroOffer)
 			{
+				this.myLastOfferIndex = null;
 				o = this.myLastOffer;
 			}
 		}
+
+		var mySumValue = 0;
+		for (var i = 0; i < o.length; ++i){
+			mySumValue += o[i] * this.values[i];
+		}
+		this.log(`my sum value: ${mySumValue}`);
 		
-		this.myLastOffer = o;
+		this.myLastOffer = o;		
+		this.rounds--;
         return o;
     }
 };
